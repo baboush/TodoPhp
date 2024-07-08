23dcac3f1dd9bb3734fa75c6fe7303ed4508aa99
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-08 15:08:04 +0200

    feat(Subscription): add subscription feature

---
+++ Core/Layout/Subscription.php
@@ -0,0 +1,22 @@
+<div class="flex flex-col md:w-1/2 w-full items-center p-4">
+  <p class="text-2xl font-bold mb-4">Inscription</p>
+  <form action="" method="post">
+    <div class="mb-4">
+      <label for="login" class="block text-gray-700 text-sm font-bold mb-2">Pseudo</label>
+      <input type="text" name="login" id="login" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="mb-4">
+      <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+      <input type="email" name="email" id="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="mb-6">
+      <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe</label>
+      <input type="password" name="password" id="password" class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+    </div>
+    <div class="flex items-center justify-between">
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+        Inscription
+      </button>
+    </div>
+  </form>
+</div>
