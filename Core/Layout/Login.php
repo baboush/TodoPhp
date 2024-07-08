8d439d158cd1f684ae6f07113e641f7aadb26ada
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-08 14:47:19 +0200

    feat(login): add form login

---
+++ Core/Layout/Login.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Core\Layout;
+
+$_SESSION['user'] = null;
+$login = $_POST['login'] ?? null;
+$password = $_POST['password'] ?? null;
+?>
+
+<div class="flex flex-row flex-wrap">
+  <div class="md:w-1/2 p-4">
+    <form action="#" method="post">
+      <div class="mb-4">
+        <label for="login" class="block text-gray-700 text-sm font-bold mb-2">Pseudo</label>
+        <input type="text" name="login" id="login " class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+      <div class="mb-6">
+        <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Mot de passe</label>
+        <input type="password" name="password" id="password" class="shadow appearance-none border border-red rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+      </div>
+      <div class="flex items-center justify-between">
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+          Connexion
+        </button>
+    </form>
+  </div>
+<?php echo $login ?>
+</div>
--- index.php
+++ index.php
@@ -3,19 +3,14 @@
 namespace App;
 
 require_once "./vendor/autoload.php";
-
-use App\Core\Bd;
-
-$connectionDb = Bd::getInstance();
-$conn = $connectionDb->connectionDb();
-
 require_once'./Core/Layout/Header.php';
-
 require_once './Core/Layout/Navigation.php';
 ?>
 
 
   <main>
+  <?php require_once './Core/Layout/Login.php'; ?>
+   
   </main>
 
 <?php require_once './Core/Layout/Footer.php';
