9f994e42728ddf4d8694b850c1a60bd4a82f1eb9
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-08 13:58:10 +0200

    feat(footer): add footer component

---
+++ Core/Layout/Footer.php
@@ -0,0 +1,6 @@
+    <footer class="flex w-full flex-row flex-wrap items-center justify-between fixed bottom-0 bg-gray-200 p-4 gap-10">
+      <p class="text-center w-full">© 2024 - Tous droits</p>
+      <a class="text-center w-full">Mentions lègales</a>
+    </footer>
+  </body>
+</html>
