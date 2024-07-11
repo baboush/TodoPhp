d1675b6f1a4c1ddfc2877ef4749d1d4860745494
Arnaud Roche <arnaud.roche.26@gmail.com>
2024-07-11 09:36:20 +0200

    feat(SnackBar): add component snackBar

---
+++ src/Core/Layout/Snackbar.php
@@ -0,0 +1,11 @@
+<div
+  id="snackbar"
+  class="flex justify-center
+  items-center p-4 w-full top-25px z-index-10 text-md text-white
+  fixed
+  rounded-lg
+  top-[1rem]
+  translate-y-[-100]
+  opacity-0
+  ">
+</div>
